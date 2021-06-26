5d121d8feea84eff0d9d748f47c7d59e89efe242
K.Kiyokawa <koichi20110068@gmail.com>
2021-06-26 16:05:40 +0900

    chore: generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeGraphQLModule } from 'typegraphql-nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
+import { UserCrudResolver } from '../prisma/generated/type-graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeGraphQLModule.forRoot({
+      playground: process.env.NODE_ENV !== 'production',
+      introspection: true,
+      path: '/',
+      emitSchemaFile: true,
+      validate: false,
+      context: () => ({ prisma: new PrismaClient() }),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserCrudResolver],
 })
 export class AppModule {}
